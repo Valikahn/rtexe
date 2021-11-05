b5d307452dbcb43d244058a7ae2f1e6aed61ca26
Valikahn <77326211+Valikahn@users.noreply.github.com>
2021-11-05 15:08:20 +0000

    Update ins-func-1804.sh

--- ins/1804-BionicBeaver/ins-func-1804.sh
+++ ins/1804-BionicBeaver/ins-func-1804.sh
@@ -17,7 +17,35 @@ fi
 ########################
 echo 'Installing SSL Certificate'
 
+wget http://www.openssl.org/source/openssl-1.0.1g.tar.gz > /dev/null
+wget http://www.openssl.org/source/openssl-1.0.1g.tar.gz.md5 > /dev/null
+md5sum openssl-1.0.1g.tar.gz > /dev/null
+cat openssl-1.0.1g.tar.gz.md5 > /dev/null
 
+tar -xvzf openssl-1.0.1g.tar.gz > /dev/null
+cd openssl-1.0.1g > /dev/null
+
+./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl > /dev/null
+make > /dev/null
+sudo make install > /dev/null
+make install > /dev/null
+
+sudo a2enmod ssl > /dev/null
+sudo systemctl restart apache2 > /dev/null
+sudo a2enmod auth_digest > /dev/null
+sudo systemctl restart apache2 > /dev/null
+sudo a2enmod reqtimeout > /dev/null
+sudo systemctl restart apache2 > /dev/null
+
+sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.orig
+sudo cp ins/setup/apache2.conf /etc/apache2/apache2.conf
+sudo systemctl restart apache2 > /dev/null
+
+sudo openssl req -nodes -newkey rsa:2048 -keyout /etc/apache2/apache.pem -out /etc/apache2/apache.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+sudo chmod 600 /etc/apache2/apache.pem
+
+sudo a2ensite default-ssl > /dev/null
+sudo systemctl reload apache2 > /dev/null
 
 
 ########################
