b6f978e313bb2d2c658a1a6ddd644559112d7acd
Valikahn <77326211+Valikahn@users.noreply.github.com>
2021-11-05 16:47:23 +0000

    Update ins-func-1804.sh

--- ins/1804-BionicBeaver/ins-func-1804.sh
+++ ins/1804-BionicBeaver/ins-func-1804.sh
@@ -23,12 +23,12 @@ sudo systemctl restart apache2 > /dev/null
 sudo a2enmod reqtimeout > /dev/null
 sudo systemctl restart apache2 > /dev/null
 
-sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.orig
-sudo cp ins/1804-BionicBeaver/setup/apache2.conf /etc/apache2/apache2.conf
+sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.orig > /dev/null
+sudo cp ins/1804-BionicBeaver/setup/apache2.conf /etc/apache2/apache2.conf > /dev/null
 sudo systemctl restart apache2 > /dev/null
 
-sudo openssl req -nodes -newkey rsa:2048 -keyout /etc/apache2/apache.pem -out /etc/apache2/apache.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
-sudo chmod 600 /etc/apache2/apache.pem
+sudo openssl req -nodes -newkey rsa:2048 -keyout /etc/apache2/apache.pem -out /etc/apache2/apache.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com" > /dev/null
+sudo chmod 600 /etc/apache2/apache.pem > /dev/null
 
 sudo a2ensite default-ssl > /dev/null
 sudo systemctl reload apache2 > /dev/null
