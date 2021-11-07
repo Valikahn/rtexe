c0568c59f20e640a483cfd14e203714bc3cd2902
Valikahn <77326211+Valikahn@users.noreply.github.com>
2021-11-07 21:56:54 +0000

    Add files via upload

---
+++ code/1804-BionicBeaver/ins-func-1804.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+sleep 3
+
+
+################################
+##  Checking if user is root  ##
+################################
+if [ "$(id -u)" != "0" ]; then
+	echo
+	echo "This script must be run as root." 1>&2
+	echo
+	exit 1
+fi
+
+
+########################
+##  Install xmlrpc-c  ##
+########################
+echo 'Installing xmlrpc-c'
+mkdir -p "$HOME"/$dirxmlrpcc
+cd "$HOME"/rutorrent-auto-install/temp/xmlrpc-c
+sudo apt-get -yqq install subversion > /dev/null
+svn checkout $xmlrpcc_dl xmlrpc-c > /dev/null
+cd xmlrpc-c
+./configure --disable-cplusplus > /dev/null 2>&1
+make > /dev/null 2>&1
+make install > /dev/null 2>&1
+cd ../..
+
+
+##########################
+##  Install libtorrent  ##
+##########################
+echo 'Installing libtorrent'
+mkdir -p "$HOME"/$dirlibtorrent
+cd "$HOME"/rutorrent-auto-install/temp/libtorrent
+curl -sL $libtorrent_dl -o libtorrent.tar.gz > /dev/null
+tar -zxvf libtorrent.tar.gz > /dev/null
+rm libtorrent.tar.gz
+cd libtorrent-0.13.8
+./autogen.sh > /dev/null 2>&1
+./configure > /dev/null 2>&1
+make > /dev/null 2>&1
+make install > /dev/null 2>&1
+cd ../..
+
+
+########################
+##  Install rTorrent  ##
+########################
+echo 'Installing rTorrent'
+mkdir -p "$HOME"/$dirrTorrent
+cd "$HOME"/rutorrent-auto-install/temp/rTorrent
+curl -sL $rTorrent_dl -o rtorrent.tar.gz > /dev/null
+tar -zxvf rtorrent.tar.gz > /dev/null
+rm rtorrent.tar.gz
+cd rtorrent-0.9.8
+./autogen.sh > /dev/null 2>&1
+./configure --with-xmlrpc-c > /dev/null 2>&1
+make > /dev/null 2>&1
+make install > /dev/null 2>&1
+
+cd $HOME
+
+mkdir -p rtorrent/.session
+mkdir -p rtorrent/download
+mkdir -p rtorrent/download/unpack
+mkdir -p rtorrent/watch
+
+cp -f "$HOME"/rutorrent-auto-install/config/rtorrent.rc $HOME/.rtorrent.rc
+sed -i "s|<user home>|${home}|g" $HOME/.rtorrent.rc
+sed -i "s/<user name>/$user/g" $HOME/.rtorrent.rc
+
+#########################
+##  Install ruTorrent  ##
+#########################
+echo 'Installing ruTorrent'
+sudo apt-get -yqq install rtorrent > /dev/null
+mkdir -p "$HOME"/$dirruTorrent
+cd "$HOME"/rutorrent-auto-install/temp/ruTorrent
+curl -sL $ruTorrent_dl -o ruTorrent-master.tar.gz > /dev/null
+tar -zxvf ruTorrent-master.tar.gz > /dev/null
+rm ruTorrent-master.tar.gz
+
+if [ -d /var/www/html/rutorrent ]; then
+		rm -r /var/www/html/rutorrent
+fi
+
+mv ruTorrent-master rutorrent
+cp -r rutorrent /var/www/html/
+
+chown -R www-data.www-data /var/www/html/rutorrent/
+chmod -R 775 /var/www/html/rutorrent/
+
+service apache2 restart
+systemctl reload apache2
+
+echo 'Configuring Rutorrent'
+cd
+rm /var/www/html/rutorrent/conf/config.php
+cp -f $HOME/rutorrent-auto-install/config/ruTorrent.config /var/www/html/rutorrent/conf/config.php
+mkdir -p /var/www/html/rutorrent/conf/users/$user/plugins
+cd /var/www/html/rutorrent/conf/users/$user/plugins
+
+echo "<?php" > config.php
+echo >> config.php
+echo "\$homeDirectory = \"$HOME\";" >> config.php
+echo "\$topDirectory = \"$HOME\";" >> config.php
+echo "\$scgi_port = 5000;" >> config.php
+echo "\$XMLRPCMountPoint = \"/RPC2\";" >> config.php
+echo >> config.php
+echo "?>" >> config.php
+cd ../../..
+
+sudo mv plugins.ini plugins.ini.orig
+sudo cp -f $HOME/rutorrent-auto-install/config/ruTorrent.ini /var/www/html/rutorrent/conf/plugins.ini
+sudo cp -f $HOME/rutorrent-auto-install/config/rtorrent-init /etc/init.d/rtorrent-init
+
+chmod +x /etc/init.d/rtorrent-init
+sed -i "s/<user name>/$user/g" /etc/init.d/rtorrent-init
+update-rc.d rtorrent-init defaults
+service apache2 restart
+
+sudo service apache2 restart
+sudo /etc/init.d/rtorrent-init start
+
+########################
+## Installing Webmin  ##
+########################
+#echo 'Installing Webmin'
+#mkdir -p "$HOME"/$dirvar
+#cd "$HOME"/rutorrent-auto-install/temp/webmin
+#sudo apt-get -y update
+#sudo cp ins/1804-BionicBeaver/setup/phpinfo.php /var/www/html/phpinfo.php
+#sudo cp ins/1804-BionicBeaver/setup/php.ini /var/www/html/php.ini
+#echo 'deb http://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list
+#wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
+#sudo apt-get -y update
+#sudo apt-get -y install webmin
+
+
+#################
+## FINAL TASKS ##
+#################
+#echo "Setting permissions, Starting services"
+#chown -R www-data:www-data /var/www
+#chown -R $user:$user $home
+#
+#su $user -c '/etc/init.d/rtorrent-init restart'
+#su $user -c '/etc/init.d/rtorrent-init -i restart'
+sudo service rtorrent-init restart
---
+++ code/scripts/func.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#######################
+###  SET FUNCTIONS  ###
+#######################
+
+# Are the sites up or down where we get our packages from?
+###
+up_or_down() {
+  if [[ `wget -S -T 3 --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then return 0; else return 1; fi
+}
+
+# Function to check if user name is a valid UNIX.
+###
+valid_name(){
+    until [[ $user =~ ^[a-z][-a-z0-9_]{2,31}$ ]]
+      do
+        echo "Enter user name (lowercase, numbers, dash and underscore):"
+        read user
+      done
+}
+
+# Random password generator function
+###
+passgen() {
+	local genln=$1
+	[ -z "$genln" ] && genln=8
+	tr -dc A-Za-z0-9 < /dev/urandom | head -c ${genln} | xargs
+	}
+
+#function to set a user input password
+set_pass() {
+local authpass
+local bailoutval=0
+local passcheck
+local passcheckagain
+
+exec 3>&1 >/dev/tty
+
+echo "Enter a password (6+ chars) or leave blank to generate a random one"
+
+while [ -z $authpass ]
+do
+  echo "Please enter the new password:"
+  stty -echo
+  read passcheck
+  stty echo
+
+# check that password is valid
+  if [ -z $passcheck ]; then
+    echo "Random password generated, will be provided to user at end of script"
+    bailoutval=1
+    authpass=$(passgen)
+  elif [ ${#passcheck} -lt 6 ]; then
+    echo "password needs to be at least 6 chars long" && continue
+  else
+    echo "Enter the new password again:"
+    stty -echo
+    read passcheckagain
+    stty echo
+
+# Check both passwords match
+    if [ $passcheck != $passcheckagain ]; then
+      echo "Passwords do not match"
+    else
+      authpass=$passcheck
+    fi
+  fi
+done
+
+exec >&3-
+echo $authpass
+return $bailoutval
+}
+
+#function to determine random number between 2 numbers
+random()
+{
+    local min=$1
+    local max=$2
+    local RAND=`od -t uI -N 4 /dev/urandom | awk '{print $2}'`
+    RAND=$((RAND%((($max-$min)+1))+$min))
+    echo $RAND
+}
+
+# function to ask user for y/n response
+ask_user(){
+local answer
+while true
+  do
+    read answer
+    case $answer in [Yy]* ) return 0 ;;
+                    [Nn]* ) return 1 ;;
+                        * ) echo "Enter y or n";;
+    esac
+  done
+}
+
+# function to enter IP address
+enter_ip() {
+local ip=$1
+
+exec 3>&1 >/dev/tty
+
+while true
+  do
+    if valid_ip $ip ; then
+      echo "Your Server IP is $ip"
+      echo -n "Is this correct y/n? "
+      ask_user && break
+    else
+      echo "Invalid IP address, please try again"
+    fi
+
+    echo "enter your server's IP address"
+    echo "e.g. 213.0.113.113"
+    read ip
+  done
+
+exec >&3-
+
+echo $ip
+}
