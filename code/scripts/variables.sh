1aa1583b32035d7dccb0d616781c751833114dbe
Valikahn <git@insentrica.net>
2021-11-15 21:17:44 +0000

    v2.15.1-Nephthys

--- code/scripts/variables.sh
+++ code/scripts/variables.sh
@@ -18,7 +18,7 @@ ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
 
 # Version Control
-SCRIPTVERSION="v1.15.8"
+SCRIPTVERSION="v2.15.1"
 REVDATE="15 November 2021"
 GITHUB="https://github.com/Valikahn/rtexe"
 #---------------------------------------------------------------------------------------------------------#
